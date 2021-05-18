23535afd71595d4282c5081d1e311f08d7d75bbc
nayariml <74561810+nayariml@users.noreply.github.com>
2021-05-17 23:10:21 -0300

    Add files via upload

---
+++ tools/joint_location.py
@@ -0,0 +1,44 @@
+"""
+#####################
+
+Pickle load: 
+
+all_kps = preds['kps']
+joints = preds ['joints'] #image coordinates
+poses= preds['poses']
+cams= preds['cams']
+verts = preds['verts']
+shapes = preds['shapes']
+omegas = preds['omegas']
+
+dictionary increased '_delta' is the results from time t to t+1
+#####################
+"""
+
+#Alphapose output
+Alpha2Dkps = {
+    0: "Nose", 1: "LEye", 2: "REye",  3: "LEar", 4: "REar",
+    5: "LShoulder", 6: "RShoulder", 7: "LElbow", 8: "RElbow", 9: "LWrist",
+    10:"RWrist", 11:"LHip", 12:"RHip", 13:"LKnee", 14:"RKnee",15:"LAnkle",
+    16:"RAnkle", 17:"Head", 18: "Neck", 19:"Hip", 20:"LBigToe",
+    21:"RBigToe", 22:"LSmallToe", 23:"RSmallToe", 24:"LHeel", 25: "RHeel"
+}
+
+#OpenPose output
+Open2Dkps = {
+    0: "Nose", 1: "Neck", 2: "RShoulder",  3: "RElbow", 4: "RWrist",
+    5: "LShoulder", 6: "LElbow", 7: "LWrist", 8: "MidHip", 9: "RHip",
+    10:"RKnee", 11:"RAnkle", 12:"LHip", 13:"LKnee", 14:"LAnkle",15:"REye",
+    16:"LEye", 17:"REar", 18: "LEar", 19:"LBigToe", 20:"LSmallToe",
+    21:"LHeel", 22:"RBigToe", 23:"RSmallToe", 24:"RHeel"
+}
+
+#HMMR output
+hmmr3Djoints= {
+    0: 'Right_heel', 1: 'Right_knee', 2: 'Right_hip', 3: 'Left_hip', 4: 'Left_knee',
+    5: 'Left_heel', 6: 'Right_wrist',7: 'Right_elbow', 8: 'Right_shoulder', 9: 'Left_shoulder',
+    10: 'Left_elbow', 11: 'Left_wrist', 12: 'Neck', 13: 'Head_top', 14: 'Nose', 15: 'Left_eye',
+    16: 'Right_eye', 17: 'Left_ear', 18: 'Right_ear', 19: 'Left_big_toe', 20: 'Right_big_toe',
+    21: 'Left_small_toe', 22: 'Right_small_toe', 23: 'Left_ankle', 24: 'Right_ankle'
+}
+
